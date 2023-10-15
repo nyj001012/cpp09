7722282d80854b9c5362798cb38a5cb64e0fd492
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-15 12:09:43 +0900

    feat(ex00): add initial files

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 12:09:16 by yena              #+#    #+#             */
+/*   Updated: 2023/10/15 12:09:16 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//
+// Created by 나예진 on 2023/10/15.
+//
+
+#ifndef EX00__BITCOINEXCHANGE_HPP_
+#define EX00__BITCOINEXCHANGE_HPP_
+
+class BitcoinExchange {
+
+};
+
+#endif //EX00__BITCOINEXCHANGE_HPP_
