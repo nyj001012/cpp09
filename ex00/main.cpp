85fb9ad533ec7872dc203f49b69c1347131fa56d
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-15 15:09:00 +0900

    feat(ex00): implement main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,26 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 12:06:29 by yena              #+#    #+#             */
-/*   Updated: 2023/10/15 12:06:29 by yena             ###   ########.fr       */
+/*   Updated: 2023/10/15 15:03:47 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Database.hpp"
+#include "BitcoinExchange.hpp"
+#include "font.hpp"
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << F_RED << "Error: could not open file." << FB_DEFAULT << std::endl;
+    return 1;
+  }
+  try {
+    BitcoinExchange exchanger;
+    std::map<std::string, float> data = Database::readDatabase();
+    exchanger.readBitcoinData(argv[1]);
+    exchanger.printPrice(data);
+  } catch (std::exception &e) {
+    std::cout << F_RED << e.what() << FB_DEFAULT << std::endl;
+  }
   return 0;
 }
