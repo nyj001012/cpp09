fffa1996f8ab5370d299b9ab26d4052d79dba249
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-16 15:11:51 +0900

    feat(ex02): implement prototype of sort, print function

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 14:02:21 by yena              #+#    #+#             */
-/*   Updated: 2023/10/16 14:51:28 by yena             ###   ########.fr       */
+/*   Updated: 2023/10/16 15:10:28 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,10 +48,58 @@ std::list<int> PmergeMe::getList() const {
   return this->_list;
 }
 
+int PmergeMe::getElementCount() const {
+  return this->_element_count;
+}
+
 void PmergeMe::setVector(std::vector<int> vector) {
   this->_vector = vector;
 }
 
 void PmergeMe::setList(std::list<int> list) {
   this->_list = list;
+}
+
+void PmergeMe::setElementCount(int element_count) {
+  this->_element_count = element_count;
+}
+
+double sortVector(std::vector<int> vector) {
+  clock_t start, end;
+  double time;
+
+  start = clock();
+  std::sort(vector.begin(), vector.end());
+  end = clock();
+  time = static_cast<double>(end - start);
+  return time;
+}
+
+double sortList(std::list<int> list) {
+  clock_t start, end;
+  double time;
+
+  start = clock();
+  list.sort();
+  end = clock();
+  time = static_cast<double>(end - start);
+  return time;
+}
+
+void PmergeMe::sort() {
+  float vector_time = sortVector(this->getVector());
+  float list_time = sortList(this->getList());
+  this->printResult("std::vector", vector_time);
+  this->printResult("std::list", list_time);
+}
+
+void PmergeMe::printResult(std::string data_type, float time) const {
+  std::cout << "Time to process a range of\t"
+            << this->getElementCount()
+            << " elements with "
+            << data_type
+            << "\t: "
+            << time
+            << " ms"
+            << std::endl;
 }
